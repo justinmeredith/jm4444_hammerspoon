039050ad2ad43f0a3249f283f56f2236b0d01597
justinmeredith <jm.4444@icloud.com>
2023-01-23 12:29:32 -0600

    Adds the functions activate_typora and keystroke_copy_as_markdown_in_typora

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+--Activates the Typora app
+function activate_typora()
+    hs.application.launchOrFocus("Typora")
+end
+
+--Copy text in the Typora app as markdown text
+function keystroke_copy_as_markdown_in_typora()
+    activate_typora()
+    hs.eventtap.keyStroke({"shift"})
+    hs.eventtap.keyStroke({"cmd", "shift"}, "c")
+end
+
+
+
 --Fill in topics with [40 words] 3x
 --[[
     Currently, this uses the keybinding "option + 4 + 0" to launch/focus Typora then choose the 'Select All' option from Typora's Menu Bar
@@ -8,8 +22,11 @@ hs.hotkey.bind({"alt"}, "4", function()
     
         local typora = hs.appfinder.appFromName("Typora")
         local select_all = {"Edit", "Selection", "Select All"}
+        local copy_as_markdown = {"Edit", "Copy as Markdown"}
 
         typora:selectMenuItem(select_all)
+        keystroke_copy_as_markdown_in_typora()
+        --typora:selectMenuItem(copy_as_markdown)
     end)
 end)
 
