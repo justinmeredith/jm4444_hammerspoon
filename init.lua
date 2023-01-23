51d11a9c69e57b40a8febf60086040a8e33a0345
justinmeredith <jm.4444@icloud.com>
2023-01-23 14:11:07 -0600

    Added a hotkey binding that shows/hides the Music app

--- init.lua
+++ init.lua
@@ -17,6 +17,10 @@ function count_search_string_occurrences_in_large_string(search_string, large_st
     return select(2, string.gsub(large_string, search_string, ""))
 end
 
+function store_frontmost_app()
+    return hs.application.frontmostApplication()
+end
+
 
 --]*Hotkeys*[--
 
@@ -39,3 +43,17 @@ hs.hotkey.bind({"alt"}, "4", function()
     end)
 end)
 
+
+--Show/Hide the Music app
+hs.hotkey.bind({"fn"}, "f8", function()
+    local music = hs.appfinder.appFromName("Music")
+
+    if( music:isFrontmost() == false)
+    then
+        current_app_for_music_switching = store_frontmost_app()
+        hs.application.launchOrFocus("Music")
+    else
+        music:hide()
+        current_app_for_music_switching:activate()
+    end
+end)
